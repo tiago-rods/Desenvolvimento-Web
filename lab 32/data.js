19f144a9cdfe5ed9a3fcb3fe0a0e15a5de5e1cb4
tiago.rods <166041880+tiago-rods@users.noreply.github.com>
2024-09-16 14:35:45 -0300

    Add files via upload

---
+++ lab 32/data.js
@@ -0,0 +1,34 @@
+const questions = [
+    {
+        question: "Qual das pessoas a seguir levou uma cadeirada em tv nacional, ao vivo?",
+        answers: ["John Cena", "Tobey Maguire", "Pablo Marçal", "Bolsonaro"],
+        correctAnswer: 2
+    },
+    {
+        question: "O que ocorreu de marcante na data 11 de setembro?",
+        answers: ["Vocalista da Banda Foo Fighters tem filho em relacionamento extraconjugal", "Morre Silvio Santos aos 93 anos", "Aniversario da minha mãe", "Drama de Briga em Tv nacional"],
+        correctAnswer: 0
+    },
+    {
+        question: "Quantos titulos Mundiais tem o Palmeiras ",
+        answers: [
+            "1, 1931",
+            "1, 1952",
+            "1, 1951",
+            "1, 2008"
+        ],
+        correctAnswer: 2
+    }
+    {
+        question: "Quem é o maior ganhador de títulos do Mr Olimpia",
+        answers:
+        [
+            "Lee Haney e Roney Colemam",
+            "Arnols Schwartznegger e Kay Green",
+            "Cbum e Lee Haney",
+            "Guillermo Amadio e Roney Coleman"
+
+        ],
+        correctAnswer: 0
+    }
+];
